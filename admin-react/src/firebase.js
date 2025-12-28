68c8a277410508d63822cd18fa18c16d9df38bb9
yoursPrachi <yogeshbalajiearth@gmail.com>
2025-12-28 13:19:51 +0530

    Create firebase.js

---
+++ admin-react/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
